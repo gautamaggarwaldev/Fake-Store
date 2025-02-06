148843f05e349ff85e0bf42d89eb469fdf43323a
GautamAggarwalDev <gautam.aggarwal.tech@gmail.com>
2025-02-06 22:33:20 +0530

    add routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,6 +5,12 @@ import Cart from '../pages/Cart.jsx'
 import Payment from "../pages/Payment.jsx";
 import LoadingPage from "../pages/LoadingPage.jsx";
 import PaymentSuccess from "../pages/PaymentSuccess.jsx";
+import Spinwheel from "../pages/Spinwheel.jsx";
+import PaymentMethodSelection from "../pages/PaymentMethodSelection.jsx";
+import PaymentQR from "../pages/PaymentQr.jsx";
+import PaymentTimeout from "../pages/PaymentTimeout.jsx";
+import UPI from "../pages/UPI.jsx";
+import EMI from "../pages/EMI.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -20,6 +26,10 @@ export const router = createBrowserRouter([
         element: <Cart />,
     },
     {
+        path: "/payment-method",
+        element: <PaymentMethodSelection />,
+    },
+    {
         path: "/payment",
         element: <Payment />,
     },
@@ -30,5 +40,25 @@ export const router = createBrowserRouter([
     {
         path: "/payment-success",
         element: <PaymentSuccess />,
+    },
+    {
+        path: "/spinwheel",
+        element: <Spinwheel />,
+    },
+    {
+        path: "/payment-qr",
+        element: <PaymentQR />,
+    },
+    {
+        path: "/payment-timeout",
+        element: <PaymentTimeout />,
+    },
+    {
+        path: "/upi-payment",
+        element: <UPI />,
+    },
+    {
+        path: "/emi-payment",
+        element: <EMI />,
     },
 ]);
